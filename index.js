c5c62dde01caebde729e4af1af00c47d8f4f9c18
Philipp Merz <phmrch@gmail.com>
2018-10-07 16:08:18 +0200

    Added cool background to first slide

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ window.onscroll = function () {
   }
   c = currentScrollTop;
 };
+
+(function (canvas, ctx){
+  //how much offset every second line should have
+  let offset = 50
+  //height difference between each line
+  let deltaY = 50;
+  //distance between each word on x-axis
+  let deltaX = 100;
+
+  let color = "#fff";
+  let font = "25px sans";
+
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  ctx.font = font;
+  ctx.fillStyle = color;
+  let offsetTracker = 0;
+  for (let y = 0; y <= canvas.height; y+=deltaY){
+    for (let x = 0; x < canvas.width; x+=deltaX) {
+      ctx.fillText("lukas",x + offsetTracker,y);
+    }
+    offsetTracker+=offset
+    if (offsetTracker > offset) offsetTracker = 0;
+  }
+})(document.querySelector('canvas'), document.querySelector('canvas').getContext('2d'))
